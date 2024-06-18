3e8a911e0b8bf007e4891f052936f5754fed6bef
Mark Karpeles <magicaltux@gmail.com>
2024-06-19 07:10:19 +0900

    dmi tweaks & support for aws xen

--- dmi.go
+++ dmi.go
@@ -9,55 +9,45 @@ import (
 // DMI can be used to find various information about the machine
 type DMI struct {
 	Cloud           string `json:"cloud"`
-	Vendor          string `json:"sys_vendor"`
-	Product         string `json:"product_name"`
-	BoardAssetTag   string `json:"board_asset_tag"`
-	ChassisAssetTag string `json:"chassis_asset_tag"`
+	Vendor          string `json:"sys_vendor,omitempty"`
+	ProductName     string `json:"product_name,omitempty"`
+	ProductVersion  string `json:"product_version,omitempty"`
+	BoardAssetTag   string `json:"board_asset_tag,omitempty"`
+	ChassisAssetTag string `json:"chassis_asset_tag,omitempty"`
 }
 
 func ReadDMI() (*DMI, error) {
-	var err error
-	dmi := &DMI{}
+	dmi := &DMI{Cloud: "unknown"}
 
-	dmi.Vendor, err = readDMI("sys_vendor")
-	if err != nil {
-		dmi.Cloud = "unknown"
-		return dmi, err
-	}
+	dmi.Vendor = readDMI("sys_vendor")
+	dmi.BoardAssetTag = readDMI("board_asset_tag")     // a uuid on google, the instance id on Amazon EC2
+	dmi.ChassisAssetTag = readDMI("chassis_asset_tag") // "Amazon EC2" on aws, nothing on google
+	// product_name on aws: m5a.large on google: Google Compute Engine
+	dmi.ProductName = readDMI("product_name")
+	dmi.ProductVersion = readDMI("product_version")
 
 	switch dmi.Vendor {
 	case "Amazon EC2":
 		dmi.Cloud = "aws"
 	case "Google":
 		dmi.Cloud = "gcp"
-	default:
-		// Run this on the machine & send it as an issue to add support to that cloud provider
-		// for foo in /sys/class/dmi/id/*; do echo $foo; cat $foo; done
-		dmi.Cloud = "unknown"
+	case "Xen":
+		// ProductVersion = 4.11.amazon
+		if strings.Contains(dmi.ProductVersion, "amazon") {
+			dmi.Cloud = "aws"
+			break
+		}
 	}
-
-	dmi.BoardAssetTag, err = readDMI("board_asset_tag") // a uuid on google, the instance id on Amazon EC2
-	if err != nil {
-		return dmi, err
-	}
-	dmi.ChassisAssetTag, err = readDMI("chassis_asset_tag") // "Amazon EC2" on aws, nothing on google
-	if err != nil {
-		return dmi, err
-	}
-
-	// product_name on aws: m5a.large on google: Google Compute Engine
-	dmi.Product, err = readDMI("product_name")
-	if err != nil {
-		return dmi, err
-	}
+	// if Cloud=unknown, run this on the machine & send it as an issue to add support to that cloud provider
+	// for foo in /sys/class/dmi/id/*; do echo $foo; cat $foo; done
 
 	return dmi, nil
 }
 
-func readDMI(name string) (string, error) {
+func readDMI(name string) string {
 	res, err := os.ReadFile(filepath.Join("/sys/class/dmi/id", name))
 	if err != nil {
-		return "", err
+		return ""
 	}
-	return strings.TrimSpace(string(res)), nil
+	return strings.TrimSpace(string(res))
 }
